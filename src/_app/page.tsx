e4575558c89b55c74088907ae7416780e34fdbc4
SIARHEI PALISHCHUK <smeshny@gmail.com>
2025-07-30 00:41:31 +0200

    Implement LanguageSwitcher and localization for home page title

--- src/_app/page.tsx
+++ src/_app/page.tsx
@@ -1,14 +1,22 @@
+import LanguageSwitcher from '@/components/LanguageSwitcher';
+
+import { useTranslations } from 'next-globe-gen';
+
 export default function Home() {
+    const t = useTranslations('home');
     return (
-        <div className='font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20'>
-            <main className='flex flex-col gap-[32px] row-start-2 items-center sm:items-start'>
-                <div>
-                    <h1>Lottery soon... 🎁 </h1>
-                </div>
-            </main>
-            <footer className='row-start-3 flex gap-[24px] flex-wrap items-center justify-center'>
-                <div> made by GuideDAO </div>
-            </footer>
-        </div>
+        <>
+            <LanguageSwitcher />
+            <div className='font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20'>
+                <main className='flex flex-col gap-[32px] row-start-2 items-center sm:items-start'>
+                    <div>
+                        <h1>{t('title')}</h1>
+                    </div>
+                </main>
+                <footer className='row-start-3 flex gap-[24px] flex-wrap items-center justify-center'>
+                    <div> made by GuideDAO </div>
+                </footer>
+            </div>
+        </>
     );
 }

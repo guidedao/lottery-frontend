d7621be09de6e358644189e697bd75cecdac860b
SIARHEI PALISHCHUK <smeshny@gmail.com>
2025-07-29 23:48:09 +0200

    setup next-globe-gen

---
+++ i18n.config.ts
@@ -0,0 +1,9 @@
+import type { Config } from "next-globe-gen";
+
+const config: Config = {
+//   locales: ["en", "ru", "ua", "pl", "de", ], // TODO: add more locales
+  locales: ["en", "ru"],
+  defaultLocale: "en",
+};
+
+export default config;

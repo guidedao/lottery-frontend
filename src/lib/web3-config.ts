ecd95148bb25fd5b0e072f537dd03a183a2260db
SIARHEI PALISHCHUK <smeshny@gmail.com>
2025-08-27 15:48:27 +0200

    change default network to arb sepolia

--- src/lib/web3-config.ts
+++ src/lib/web3-config.ts
@@ -1,14 +1,14 @@
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
 
 import { http } from 'wagmi';
-import { holesky } from 'wagmi/chains';
+import { arbitrumSepolia } from 'wagmi/chains';
 
 export const config = getDefaultConfig({
     appName: 'GuideDAO Lottery',
     projectId: '976b8d08720eac427e1f14ba17af74b6',
-    chains: [holesky],
+    chains: [arbitrumSepolia],
     transports: {
-        [holesky.id]: http('https://ethereum-holesky-rpc.publicnode.com')
+        [arbitrumSepolia.id]: http('https://sepolia-rollup.arbitrum.io/rpc')
     },
     ssr: true // Next.js server-side rendering support
 });

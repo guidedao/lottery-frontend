a5f8edd7ba7609dedb3cbeaad99894823ac2aabf
SIARHEI PALISHCHUK <smeshny@gmail.com>
2025-08-27 15:46:39 +0200

    fix linter errors and formatting

--- src/lib/web3-config.ts
+++ src/lib/web3-config.ts
@@ -1,13 +1,14 @@
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
-import { holesky } from 'wagmi/chains';
+
 import { http } from 'wagmi';
+import { holesky } from 'wagmi/chains';
 
 export const config = getDefaultConfig({
-  appName: 'GuideDAO Lottery',
-  projectId: '976b8d08720eac427e1f14ba17af74b6',
-  chains: [holesky],
-  transports: {
-    [holesky.id]: http('https://ethereum-holesky-rpc.publicnode.com'),
-  },
-  ssr: true, // Next.js server-side rendering support
+    appName: 'GuideDAO Lottery',
+    projectId: '976b8d08720eac427e1f14ba17af74b6',
+    chains: [holesky],
+    transports: {
+        [holesky.id]: http('https://ethereum-holesky-rpc.publicnode.com')
+    },
+    ssr: true // Next.js server-side rendering support
 });

93eaa3b90dab68b8397c43454f69443dde3fbc71
SIARHEI PALISHCHUK <smeshny@gmail.com>
2025-09-11 12:56:07 +0200

    Refactor WalletControls component to remove chain selection and update Web3Provider to use dark theme

--- src/providers/Web3Provider.tsx
+++ src/providers/Web3Provider.tsx
@@ -2,7 +2,7 @@
 
 import { projectConfig } from '@/config/projectConfig';
 import { config } from '@/lib/web3-config';
-import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { RainbowKitProvider, darkTheme } from '@rainbow-me/rainbowkit';
 import { type GetSiweMessageOptions, RainbowKitSiweNextAuthProvider } from '@rainbow-me/rainbowkit-siwe-next-auth';
 import '@rainbow-me/rainbowkit/styles.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
@@ -27,7 +27,7 @@ export function Web3Provider({ children, session }: Web3ProviderProps) {
             <SessionProvider refetchInterval={0} session={session}>
                 <QueryClientProvider client={queryClient}>
                     <RainbowKitSiweNextAuthProvider getSiweMessageOptions={getSiweMessageOptions}>
-                        <RainbowKitProvider>{children}</RainbowKitProvider>
+                        <RainbowKitProvider theme={darkTheme()}>{children}</RainbowKitProvider>
                     </RainbowKitSiweNextAuthProvider>
                 </QueryClientProvider>
             </SessionProvider>

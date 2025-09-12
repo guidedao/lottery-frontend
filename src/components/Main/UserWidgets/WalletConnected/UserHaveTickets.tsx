eb0fbf1bfcee34b8130ae48a60948c5493f2bb92
SIARHEI PALISHCHUK <smeshny@gmail.com>
2025-09-12 14:04:15 +0200

    Update gap spacing in UserHaveTickets

--- src/components/Main/UserWidgets/WalletConnected/UserHaveTickets.tsx
+++ src/components/Main/UserWidgets/WalletConnected/UserHaveTickets.tsx
@@ -37,7 +37,7 @@ export default function UserHaveTickets() {
 
     return (
         <section className='flex flex-col gap-6 lg:gap-6 w-full lg:w-1/2 lg:self-stretch'>
-            <article className='surface-glass flex flex-col flex-1 h-full min-h-[220px] lg:min-h-[260px] basis-full p-6 rounded-xl gap-10'>
+            <article className='surface-glass flex flex-col flex-1 h-full min-h-[220px] lg:min-h-[260px] basis-full p-6 rounded-xl gap-6'>
                 <h2 className='text-2xl font-bold text-foreground'>Buy more lottery tickets</h2>
 
                 <TicketPurchaseRow

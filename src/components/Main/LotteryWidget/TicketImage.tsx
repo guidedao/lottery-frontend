b98160fe9b7472c3ed1447d2f8147d50ed408093
SIARHEI PALISHCHUK <smeshny@gmail.com>
2025-09-12 14:25:19 +0200

    Add glass effect and animations to TicketImage component

--- src/components/Main/LotteryWidget/TicketImage.tsx
+++ src/components/Main/LotteryWidget/TicketImage.tsx
@@ -4,13 +4,20 @@ import Image from 'next/image';
 
 const TicketImage = () => {
     return (
-        <Image
-            src='/images/lottery-ticket.webp'
-            alt='guide dao lottery ticket'
-            width={300}
-            height={400}
-            className='w-[90%] md:w-[80%] lg:w-full h-auto'
-        />
+        <div className='relative w-[90%] md:w-[80%] lg:w-full animate-glass-float overflow-hidden rounded-2xl'>
+            <Image
+                src='/images/lottery-ticket.webp'
+                alt='guide dao lottery ticket'
+                width={300}
+                height={400}
+                className='w-full h-auto rounded-2xl shadow-lg'
+                priority
+            />
+            {/* Glass overlay */}
+            <div className='glass-overlay rounded-xl'>
+                <div className='glass-sheen' />
+            </div>
+        </div>
     );
 };
 
